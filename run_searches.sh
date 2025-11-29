e2880faa606ba613b018928abf2eeacede393fd1
aind workspaces <aind-workspaces@udacity.com>
2025-11-29 22:56:13 +0000

    Adding prob 4 set level results

--- run_searches.sh
+++ run_searches.sh
@@ -8,7 +8,17 @@
 #python run_search.py -p 4 -s 7 > p4_s7.txt 2>&1
 #echo "Running p4s9"
 #python run_search.py -p 4 -s 9 > p4_s9.txt 2>&1
-echo "Running p4s10"
-python run_search.py -p 4 -s 10 > p4_s10.txt 2>&1
+#echo "Running p4s10"
+#python run_search.py -p 4 -s 10 > p4_s10.txt 2>&1
 #echo "Running p4s11"
 #python run_search.py -p 4 -s 11 > p4_s11.txt 2>&1
+
+# Rerun set_level heuristic searches since we optimized
+# the algorithm.
+#echo "Running p1s11"
+#python run_search.py -p 1 -s 11 > p1_s11.txt 2>&1
+for p in 4; do
+	echo "Running p${p}s11"
+    # echo python run_search.py -p ${p} -s 11 > p${p}_s11.txt
+	python run_search.py -p ${p} -s 11 > p${p}_s11.txt 2>&1
+done
